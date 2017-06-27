b4738fb095a100d3f9d79146366fb05ae2d400ba
Josevlz <jose.vlzlara@gmail.com>
2017-06-26 22:38:15 -1200

    StartUp Project

---
+++ Sistema Academico/Sistema Academico/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sistema_Academico.Startup))]
+namespace Sistema_Academico
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
